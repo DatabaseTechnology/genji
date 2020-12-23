6bb7307e4b08c80eabc0a14e2edc0d7e8c8474fc
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-12-23 16:51:02 +0400

    Add iterators

--- document/stream/stream.go
+++ document/stream/stream.go
@@ -2,6 +2,7 @@ package stream
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/genjidb/genji/sql/query/expr"
 )
@@ -9,28 +10,27 @@ import (
 // ErrStreamClosed is used to indicate that a stream must be closed.
 var ErrStreamClosed = errors.New("stream closed")
 
-// An Iterator can iterate over values.
-type Iterator interface {
-	// Iterate goes through all the values and calls the given function by passing each one of them.
-	// If the given function returns an error, the iteration stops.
-	Iterate(fn func(env *expr.Environment) error) error
-}
-
 // An Operator is used to modify a stream.
-// If an operator returns a value, it will be passed to the next operator.
-// If it returns a nil value, the value will be ignored.
+// It takes an environment containing the current value as well as any other metadata and returns
+// a new environment which will be passed to the next operator.
+// If it returns an environment with no value, the env will be ignored.
 // If it returns an error, the stream will be interrupted and that error will bubble up
 // and returned by this function, unless that error is ErrStreamClosed, in which case
 // the Iterate method will stop the iteration and return nil.
-// Stream operators can be reused, and thus, any state or side effect should be kept within the operator closure
+// Stream operators can be reused, and thus, any state or side effect should be kept within the Op closure
 // unless the nature of the operator prevents that.
-type Operator func() func(env *expr.Environment) (*expr.Environment, error)
+type Operator interface {
+	Op() func(env *expr.Environment) (*expr.Environment, error)
+}
 
+// Stream reads values from an iterator one by one and passes them
+// through a list of operators for transformation.
 type Stream struct {
 	it Iterator
 	op Operator
 }
 
+// New creates a stream using the given iterator.
 func New(it Iterator) Stream {
 	return Stream{
 		it: it,
@@ -38,7 +38,7 @@ func New(it Iterator) Stream {
 }
 
 // Pipe creates a new Stream who can read its data from s and apply
-// op to every document passed by its Iterate method.
+// op to every value passed by its Iterate method.
 func (s Stream) Pipe(op Operator) Stream {
 	return Stream{
 		it: s,
@@ -48,10 +48,8 @@ func (s Stream) Pipe(op Operator) Stream {
 
 // Iterate calls the underlying iterator's iterate method.
 // If this stream was created using the Pipe method, it will apply fn
-// to any document passed by the underlying iterator.
-// If fn returns a document, it will be passed to the next stream.
-// If it returns a nil document, the document will be ignored.
-// If it returns an error, the stream will be interrupted and that error will bubble up
+// to any value passed by the underlying iterator.
+// If fn returns an error, the stream will be interrupted and that error will bubble up
 // and returned by fn, unless that error is ErrStreamClosed, in which case
 // the Iterate method will stop the iteration and return nil.
 // It implements the Iterator interface.
@@ -64,7 +62,7 @@ func (s Stream) Iterate(fn func(env *expr.Environment) error) error {
 		return s.it.Iterate(fn)
 	}
 
-	opFn := s.op()
+	opFn := s.op.Op()
 
 	err := s.it.Iterate(func(env *expr.Environment) error {
 		env, err := opFn(env)
@@ -81,15 +79,18 @@ func (s Stream) Iterate(fn func(env *expr.Environment) error) error {
 	return nil
 }
 
+// A MapOperator applies an expression on each value of the stream and returns a new value.
 type MapOperator struct {
 	E expr.Expr
 }
 
-func Map(e expr.Expr) Operator {
-	return (&MapOperator{E: e}).Operator
+// Map creates a MapOperator.
+func Map(e expr.Expr) *MapOperator {
+	return &MapOperator{E: e}
 }
 
-func (m *MapOperator) Operator() func(env *expr.Environment) (*expr.Environment, error) {
+// Op implements the Operator interface.
+func (m *MapOperator) Op() func(env *expr.Environment) (*expr.Environment, error) {
 	var newEnv expr.Environment
 
 	return func(env *expr.Environment) (*expr.Environment, error) {
@@ -102,4 +103,8 @@ func (m *MapOperator) Operator() func(env *expr.Environment) (*expr.Environment,
 		newEnv.Outer = env
 		return &newEnv, nil
 	}
+}
+
+func (m *MapOperator) String() string {
+	return fmt.Sprintf("map(%s)", m.E)
 }
